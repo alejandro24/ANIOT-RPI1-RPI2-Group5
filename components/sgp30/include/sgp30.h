e02338662ba17072d945c40cb05f2b82f7868036
Pablo C. Alcalde <paalcald@gmail.com>
2024-12-06 00:08:53 +0100

    [UNTESTED] sgp30 measurement and initialization implemented

--- components/sgp30/include/sgp30.h
+++ components/sgp30/include/sgp30.h
@@ -1,13 +1,14 @@
-#ifndef SPG30_H
-#define SPG30_H
+#ifndef SGP30_H
+#define SGP30_H
 
 #include "esp_err.h"
 #include "esp_event.h"
 #include "esp_event_base.h"
 #include "driver/i2c_master.h"
+#include <stdint.h>
 
-#define SPG30_I2C_ADDR ((uint8_t) 0x58) /* I2C address of SPG30 sensor */
-#define SPG30_CRC_8_POLY ((uint8_t) 0x31) /* CRC-8 generator polynomial */
+#define SGP30_I2C_ADDR ((uint8_t) 0x58) /* I2C address of SGP30 sensor */
+#define SGP30_CRC_8_POLY ((uint8_t) 0x31) /* CRC-8 generator polynomial */
 
 typedef enum {
     SENSOR_EVENT_DISTANCE_NEW,
@@ -32,30 +33,30 @@ typedef enum {
 ESP_EVENT_DECLARE_BASE(SENSOR_EVENTS);
 
 /**
- * @brief Creates a handle for the SPG30 device on the specified I2C bus.
+ * @brief Creates a handle for the SGP30 device on the specified I2C bus.
  *
- * This function initializes and returns a handle for the SPG30 sensor device
+ * This function initializes and returns a handle for the SGP30 sensor device
  * connected to the given I2C bus. The device address and communication speed
  * must be specified.
  *
- * @param bus_handle Handle to the I2C bus where the SPG30 device is connected.
- * @param dev_addr I2C address of the SPG30 device.
+ * @param bus_handle Handle to the I2C bus where the SGP30 device is connected.
+ * @param dev_addr I2C address of the SGP30 device.
  * @param dev_speed Communication speed for the I2C device.
- * @return Handle to the SPG30 device, or NULL if creation failed.
+ * @return Handle to the SGP30 device, or NULL if creation failed.
  */
 i2c_master_dev_handle_t sgp30_device_create(
-    i2m_master_bus_handle_t bus_handle,
+    i2c_master_bus_handle_t bus_handle,
     const uint16_t dev_addr,
     const uint32_t dev_speed);
 
 /**
- * @brief Deletes the SPG30 device instance.
+ * @brief Deletes the SGP30 device instance.
  *
- * This function releases any resources associated with the SPG30 device
+ * This function releases any resources associated with the SGP30 device
  * instance identified by the provided I2C master device handle.
  *
- * @param dev_handle The handle to the I2C master device associated with the 
- * SPG30 device.
+ * @param dev_handle The handle to the I2C master device associated with the
+ * SGP30 device.
  *
  * @return
  *     - ESP_OK: Success
@@ -65,39 +66,54 @@ i2c_master_dev_handle_t sgp30_device_create(
 esp_err_t sgp30_device_delete(i2c_master_dev_handle_t dev_handle);
 
 /**
- * @brief Retrieve temperature and humidity readings from the SPG30 sensor.
+ * @brief Initiates the measurement capabilities of the SGP30 device.
  *
- * This function communicates with the SPG30 sensor over I2C to obtain the current
+ * This function has to be executed once before any measurement can be issued
+ *
+ * @param bus_handle Handle to the I2C bus where the SGP30 device is connected.
+ * @return
+ *     - ESP_OK: Success
+ *     - ESP_ERR_INVALID_ARG: Invalid arguments
+ *     - ESP_FAIL: Communication with the sensor failed
+ *     - ESP_ERR_INVALID_CRC: Received wrong chechsum
+ */
+esp_err_t sgp30_init_air_quality(i2c_master_dev_handle_t dev_handle);
+
+/**
+ * @brief Retrieve eCO2 and TVOC readings from the SGP30 sensor.
+ *
+ * This function communicates with the SGP30 sensor over I2C to obtain the current
  * temperature and humidity measurements.
  *
  * @param[in] dev_handle Handle to the I2C master device.
  * @param[in] reg Register to read from.
- * @param[out] data1 Pointer to a float where the temperature reading will be stored.
- * @param[out] data2 Pointer to a float where the humidity reading will be stored.
+ * @param[out] data1 Pointer to a uint16_t where the eCO2 reading will be stored.
+ * @param[out] data2 Pointer to a uint16_t where the TVOC reading will be stored.
  *
  * @return
  *     - ESP_OK: Success
  *     - ESP_ERR_INVALID_ARG: Invalid arguments
  *     - ESP_FAIL: Communication with the sensor failed
+ *     - ESP_ERR_INVALID_CRC: Received wrong chechsum
  */
-esp_err_t sgp30_get_th(
+esp_err_t sgp30_measure_air_quality(
     i2c_master_dev_handle_t dev_handle,
-    shtc3_register_rw_t reg,
-    float *data1,
-    float *data2);
+    uint16_t *data1,
+    uint16_t *data2);
 
 /**
- * @brief Retrieve the ID from the SPG30 sensor.
+ * @brief Retrieve the ID from the SGP30 sensor.
  *
- * This function communicates with the SPG30 sensor over I2C to obtain its unique identifier.
+ * This function communicates with the SGP30 sensor over I2C to obtain its unique identifier.
  *
  * @param dev_handle Handle to the I2C master device.
  * @param id Pointer to a buffer where the ID will be stored.
- * 
- * @return 
+ *
+ * @return
  *     - ESP_OK: Success
  *     - ESP_ERR_INVALID_ARG: Invalid argument
  *     - ESP_FAIL: Communication with the sensor failed
+ *     - ESP_ERR_INVALID_CRC: Received wrong chechsum
  */
 esp_err_t sgp30_get_id(i2c_master_dev_handle_t dev_handle, uint8_t *id);
 
--- components/sgp30/sgp30.c
+++ components/sgp30/sgp30.c
@@ -1,18 +1,45 @@
-#include "esp_err.h"
 #include "esp_check.h"
 #include "esp_event.h"
 #include "esp_timer.h"
 #include "esp_event_base.h"
 #include "esp_err.h"
 #include "esp_log.h"
+#include <stdint.h>
 #include "sgp30.h"
 
 ESP_EVENT_DEFINE_BASE(SENSOR_EVENTS);
 
 static char* TAG = "SGP30";
-
+// This is a very inefficient
 static esp_err_t crc8_check(uint8_t *buffer, size_t size) {
     uint8_t crc = 0;
+
+    for (int i = 0; i < size; i++) {
+        // We XOR the first/next bit into the crc
+        crc ^= buffer[i];
+        for (int j = 0; j < 8; j++) {
+            // Check if the MSB is set to 1
+            if (crc & 0x80) {
+                // If it is set to 1 get rid of the 1 and XOR
+                crc = (crc << 1) ^ SGP30_CRC_8_POLY;
+            } else {
+                // If it is not, just keep looking for a 1
+                crc <<= 1;
+            }
+        }
+    }
+
+    // Error if reminder is not zero.
+    if (crc) {
+        return ESP_ERR_INVALID_CRC;
+    } else {
+        return ESP_OK;
+    }
+}
+
+static uint8_t crc8_gen(uint8_t *buffer, size_t size) {
+    uint8_t crc = 0;
+
     for (int i = 0; i < size; i++) {
         crc ^= buffer[i];
         for (int j = 0; j < 8; j++) {
@@ -23,9 +50,11 @@ static esp_err_t crc8_check(uint8_t *buffer, size_t size) {
             }
         }
     }
+
+    return crc;
 }
 
-i2c_master_dev_handle_t shtc3_device_create(
+i2c_master_dev_handle_t sgp30_device_create(
     i2c_master_bus_handle_t bus_handle,
     const uint16_t dev_addr,
     const uint32_t dev_speed)
@@ -41,42 +70,113 @@ i2c_master_dev_handle_t shtc3_device_create(
     // Add device to the I2C bus
     ESP_ERROR_CHECK(
         i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle)
-    );:
+    );
 
     return dev_handle;
 }
 
-esp_err_t shtc3_device_delete(i2c_master_dev_handle_t dev_handle)
+esp_err_t sgp30_device_delete(i2c_master_dev_handle_t dev_handle)
 {
     return i2c_master_bus_rm_device(dev_handle);
 }
 
-esp_err_t shtc3_get_id(i2c_master_dev_handle_t dev_handle, uint8_t *id)
+esp_err_t sgp30_init_air_quality(i2c_master_dev_handle_t dev_handle)
 {
-    shtc3_register_rw_t write_addr = SGP30_REG_READ_ID;
+    sgp30_register_rw_t write_addr = SGP30_REG_INIT_AIR_QUALITY;
+    uint8_t write_buffer[2];
+    write_buffer[0] = write_addr & 0xFF; /* Mask should be unnecessary */
+    write_buffer[1] = (write_addr >> 8) & 0xFF; /* Mask should be unnecessary */
+
+    ESP_RETURN_ON_ERROR(
+        i2c_master_transmit(dev_handle, write_buffer, 2, -1),
+        TAG, "I2C init air quality failed"
+    );
+
+    // The sensor needs a max of 10 ms to respond to the I2C read header.
+    vTaskDelay(pdMS_TO_TICKS(1));
+
+    return ESP_OK;
+}
+
+esp_err_t sgp30_measure_air_quality(i2c_master_dev_handle_t dev_handle, uint16_t *eCO2, uint16_t *TVOC)
+{
+    sgp30_register_rw_t write_addr = SGP30_REG_MEASURE_AIR_QUALITY;
+    uint8_t write_buffer[2];
+    write_buffer[0] = write_addr & 0xFF; /* Mask should be unnecessary */
+    write_buffer[1] = (write_addr >> 8) & 0xFF; /* Mask should be unnecessary */
+
+    /* We return 2 words (16 bits/w) each followed by an 8 bit CRC checksum */
+    /* Therefore we need to save 48 bits for the received data */
+    uint8_t bytes_read[6];
+
+    ESP_RETURN_ON_ERROR(
+        i2c_master_transmit(dev_handle, write_buffer, 2, -1),
+        TAG, "I2C get serial id write failed"
+    );
+
+    // The sensor needs a max of 12 ms to respond to the I2C read header.
+    vTaskDelay(pdMS_TO_TICKS(12));
+
+    ESP_RETURN_ON_ERROR(
+        i2c_master_receive(dev_handle, bytes_read, 6, -1),
+        TAG, "I2C get serial id read failed"
+    );
+
+    ESP_RETURN_ON_ERROR(
+        crc8_check(bytes_read, 6),
+        TAG, "I2C get serial id CRC failed"
+    );
+
+    // Copy the read ID to the provided id pointer
+    *eCO2 = (uint16_t) (bytes_read[1] << 8) | bytes_read[0];
+    // bytes_read[2] is CRC for the first word, we can ignore it.
+
+    *TVOC = (uint16_t) (bytes_read[4] << 8) | bytes_read[3];
+    // bytes_read[5] is CRC for the second word, we can ignore it.
+    return ESP_OK;
+}
+
+esp_err_t sgp30_get_id(i2c_master_dev_handle_t dev_handle, uint8_t *id)
+{
+    sgp30_register_rw_t write_addr = SGP30_REG_GET_SERIAL_ID;
+    uint8_t write_buffer[2];
+    write_buffer[0] = write_addr & 0xFF; /* Mask should be unnecessary */
+    write_buffer[1] = (write_addr >> 8) & 0xFF; /* Mask should be unnecessary */
+
     /* We return 3 words (16 bits/w) each followed by an 8 bit CRC checksum */
     /* Therefore we need to save 72 bits for the received data */
     uint8_t bytes_read[9];
 
-    ESP_RETURN_ON_ERROR(i2c_master_transmit(dev_handle, write_addr, 2, -1));
+    ESP_RETURN_ON_ERROR(
+        i2c_master_transmit(dev_handle, write_buffer, 2, -1),
+        TAG, "I2C get serial id write failed"
+    );
 
     // The sensor needs .5 ms to respond to the I2C read header.
-    xTaskDelay(pdMS_TO_TICKS(1));
+    vTaskDelay(pdMS_TO_TICKS(1));
 
-    ESP_RETURN_ON_ERROR(i2c_master_receive(dev_handle, read_reg, 9, -1));
+    ESP_RETURN_ON_ERROR(
+        i2c_master_receive(dev_handle, bytes_read, 9, -1),
+        TAG, "I2C get serial id read failed"
+    );
 
-    ESP_RETURN_ON_ERROR(crc8_check(bytes_read, 9));
+    ESP_RETURN_ON_ERROR(
+        crc8_check(bytes_read, 9),
+        TAG, "I2C get serial id CRC failed"
+    );
 
     // Copy the read ID to the provided id pointer
     id[0] = bytes_read[0];
     id[1] = bytes_read[1];
-    // bytes_read[2] is CRC for the first wor, we can ignore it.
+    // bytes_read[2] is CRC for the first word, we can ignore it.
+
     id[2] = bytes_read[3];
     id[3] = bytes_read[4];
-    // bytes_read[5] is CRC for the second wor, we can ignore it.
+    // bytes_read[5] is CRC for the second word, we can ignore it.
+
     id[4] = bytes_read[6];
     id[5] = bytes_read[7];
     // bytes_read[8] is CRC for the third word, we can ignore it.
 
-    return ret;
+    return ESP_OK;
 }
