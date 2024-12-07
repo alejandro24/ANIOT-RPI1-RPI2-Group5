6e0cfcfe25a42e7530c129bf820dc1068d35b80f
Pelli223 <dpelli.lafu@gmail.com>
2024-12-07 22:06:14 +0100

    Se añade el componente de provissionamiento, csv de particion de nvs y script de python para el provisionamiento

---
+++ components/softAP_provision/softAP_provision.c
@@ -0,0 +1,226 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/event_groups.h>
+
+#include <esp_log.h>
+#include <esp_wifi.h>
+#include <esp_event.h>
+#include <nvs_flash.h>
+
+#include <wifi_provisioning/manager.h>
+
+#include <wifi_provisioning/scheme_softap.h>
+#include "qrcode.h"
+#include "softAP_provision.h"
+
+static const char *TAG = "softAP_provisioning";
+static esp_event_loop_handle_t softAP_provision_event_loop;
+static char thingsboard_url[100];
+
+#if CONFIG_EXAMPLE_PROV_SECURITY_VERSION_2
+static esp_err_t example_get_sec2_salt(const char **salt, uint16_t *salt_len) {
+#if CONFIG_EXAMPLE_PROV_SEC2_DEV_MODE
+    ESP_LOGI(TAG, "Development mode: using hard coded salt");
+    *salt = sec2_salt;
+    *salt_len = sizeof(sec2_salt);
+    return ESP_OK;
+#elif CONFIG_EXAMPLE_PROV_SEC2_PROD_MODE
+    ESP_LOGE(TAG, "Not implemented!");
+    return ESP_FAIL;
+#endif
+}
+
+static esp_err_t example_get_sec2_verifier(const char **verifier, uint16_t *verifier_len) {
+#if CONFIG_EXAMPLE_PROV_SEC2_DEV_MODE
+    ESP_LOGI(TAG, "Development mode: using hard coded verifier");
+    *verifier = sec2_verifier;
+    *verifier_len = sizeof(sec2_verifier);
+    return ESP_OK;
+#elif CONFIG_EXAMPLE_PROV_SEC2_PROD_MODE
+    /* This code needs to be updated with appropriate implementation to provide verifier */
+    ESP_LOGE(TAG, "Not implemented!");
+    return ESP_FAIL;
+#endif
+}
+#endif
+
+static void wifi_init_sta(void)
+{
+    /* Start Wi-Fi in station mode */
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_start());
+}
+
+static void get_device_service_name(char *service_name, size_t max)
+{
+    uint8_t eth_mac[6];
+    const char *ssid_prefix = "PROV_";
+    esp_wifi_get_mac(WIFI_IF_STA, eth_mac);
+    snprintf(service_name, max, "%s%02X%02X%02X",
+             ssid_prefix, eth_mac[3], eth_mac[4], eth_mac[5]);
+}
+
+/* Handler for the optional provisioning endpoint registered by the application.
+ * The data format can be chosen by applications. Here, we are using plain ascii text.
+ * Applications can choose to use other formats like protobuf, JSON, XML, etc.
+ * Note that memory for the response buffer must be allocated using heap as this buffer
+ * gets freed by the protocomm layer once it has been sent by the transport layer.
+ */
+esp_err_t thingsboard_url_prov_data_handler(uint32_t session_id, const uint8_t *inbuf, ssize_t inlen,
+                                          uint8_t **outbuf, ssize_t *outlen, void *priv_data)
+{
+    if (inbuf) {
+        ESP_LOGI(TAG, "Received data: %.*s", inlen, (char *)inbuf);
+        snprintf(thingsboard_url, sizeof(thingsboard_url), "%.*s", (int)inlen, (char *)inbuf);
+    }
+    char response[] = "SUCCESS";
+    *outbuf = (uint8_t *)strdup(response);
+    if (*outbuf == NULL) {
+        ESP_LOGE(TAG, "System out of memory");
+        return ESP_ERR_NO_MEM;
+    }
+    *outlen = strlen(response) + 1; /* +1 for NULL terminating byte */
+
+    return ESP_OK;
+}
+
+static void wifi_prov_print_qr(const char *name, const char *username, const char *pop, const char *transport)
+{
+    if (!name || !transport) {
+        ESP_LOGW(TAG, "Cannot generate QR code payload. Data missing.");
+        return;
+    }
+    char payload[150] = {0};
+    if (pop) {
+#if CONFIG_EXAMPLE_PROV_SECURITY_VERSION_1
+        snprintf(payload, sizeof(payload), "{\"ver\":\"%s\",\"name\":\"%s\"" \
+                    ",\"pop\":\"%s\",\"transport\":\"%s\"}",
+                    PROV_QR_VERSION, name, pop, transport);
+#elif CONFIG_EXAMPLE_PROV_SECURITY_VERSION_2
+        snprintf(payload, sizeof(payload), "{\"ver\":\"%s\",\"name\":\"%s\"" \
+                    ",\"username\":\"%s\",\"pop\":\"%s\",\"transport\":\"%s\"}",
+                    PROV_QR_VERSION, name, username, pop, transport);
+#endif
+    } else {
+        snprintf(payload, sizeof(payload), "{\"ver\":\"%s\",\"name\":\"%s\"" \
+                    ",\"transport\":\"%s\"}",
+                    PROV_QR_VERSION, name, transport);
+    }
+#ifdef CONFIG_EXAMPLE_PROV_SHOW_QR
+    ESP_LOGI(TAG, "Scan this QR code from the provisioning application for Provisioning.");
+    esp_qrcode_config_t cfg = ESP_QRCODE_CONFIG_DEFAULT();
+    esp_qrcode_generate(&cfg, payload);
+#endif /* CONFIG_APP_WIFI_PROV_SHOW_QR */
+    ESP_LOGI(TAG, "If QR code is not visible, copy paste the below URL in a browser.\n%s?data=%s", QRCODE_BASE_URL, payload);
+}
+
+static void softAP_provision_init(esp_event_loop_handle_t loop){
+    /* Initialize TCP/IP */
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    /* Initialize Wi-Fi including netif with default config */
+    esp_netif_create_default_wifi_sta();
+    /* Initialize Wi-Fi including netif with default config */
+    esp_netif_create_default_wifi_sta();
+    esp_netif_create_default_wifi_ap();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+        /* Configuration for the provisioning manager */
+    wifi_prov_mgr_config_t config = {
+        .scheme = wifi_prov_scheme_softap,
+        /*This can be set to
+         * WIFI_PROV_EVENT_HANDLER_NONE when using wifi_prov_scheme_softap*/
+        .scheme_event_handler = WIFI_PROV_EVENT_HANDLER_NONE
+    };
+    /* Initialize provisioning manager with the
+     * configuration parameters set above */
+    ESP_ERROR_CHECK(wifi_prov_mgr_init(config));
+    
+    char service_name[12];
+    get_device_service_name(service_name, sizeof(service_name));
+
+    #ifdef CONFIG_EXAMPLE_PROV_SECURITY_VERSION_1
+        /* What is the security level that we want (1, 2):
+         *      - WIFI_PROV_SECURITY_0 is simply plain text communication.
+         *      - WIFI_PROV_SECURITY_1 is secure communication which consists of secure handshake
+         *          using X25519 key exchange and proof of possession (pop) and AES-CTR
+         *          for encryption/decryption of messages.
+         *      - WIFI_PROV_SECURITY_2 SRP6a based authentication and key exchange
+         *        + AES-GCM encryption/decryption of messages
+         */
+        wifi_prov_security_t security = WIFI_PROV_SECURITY_1;
+
+        /* Do we want a proof-of-possession (ignored if Security 0 is selected):
+         *      - this should be a string with length > 0
+         *      - NULL if not used
+         */
+        const char *pop = "abcd1234";
+
+        /* This is the structure for passing security parameters
+         * for the protocomm security 1.
+         */
+        wifi_prov_security1_params_t *sec_params = pop;
+
+        const char *username  = NULL;
+
+#elif CONFIG_EXAMPLE_PROV_SECURITY_VERSION_2
+        wifi_prov_security_t security = WIFI_PROV_SECURITY_2;
+        /* The username must be the same one, which has been used in the generation of salt and verifier */
+
+#if CONFIG_EXAMPLE_PROV_SEC2_DEV_MODE
+        /* This pop field represents the password that will be used to generate salt and verifier.
+         * The field is present here in order to generate the QR code containing password.
+         * In production this password field shall not be stored on the device */
+        const char *username  = EXAMPLE_PROV_SEC2_USERNAME;
+        const char *pop = EXAMPLE_PROV_SEC2_PWD;
+#elif CONFIG_EXAMPLE_PROV_SEC2_PROD_MODE
+        /* The username and password shall not be embedded in the firmware,
+         * they should be provided to the user by other means.
+         * e.g. QR code sticker */
+        const char *username  = NULL;
+        const char *pop = NULL;
+#endif
+        /* This is the structure for passing security parameters
+         * for the protocomm security 2.
+         * If dynamically allocated, sec2_params pointer and its content
+         * must be valid till WIFI_PROV_END event is triggered.
+         */
+        wifi_prov_security2_params_t sec2_params = {};
+
+        ESP_ERROR_CHECK(example_get_sec2_salt(&sec2_params.salt, &sec2_params.salt_len));
+        ESP_ERROR_CHECK(example_get_sec2_verifier(&sec2_params.verifier, &sec2_params.verifier_len));
+
+        wifi_prov_security2_params_t *sec_params = &sec2_params;
+#endif
+
+    /* What is the service key (could be NULL)
+    * This translates to :
+    *     - Wi-Fi password when scheme is wifi_prov_scheme_softap
+    *          (Minimum expected length: 8, maximum 64 for WPA2-PSK)
+    *     - simply ignored when scheme is wifi_prov_scheme_ble
+    */
+    const char *service_key = NULL;
+
+    /* An endpoint that applications create to get the
+    *thingsboard url.
+    * This call must be made before starting the provisioning.
+    */
+    wifi_prov_mgr_endpoint_create("thingsboard-url");
+
+    /* Start provisioning service */
+    ESP_ERROR_CHECK(wifi_prov_mgr_start_provisioning(security, (const void *) sec_params, service_name, service_key));
+
+    /* The handler for the optional endpoint created above.
+    * This call must be made after starting the provisioning, and only if the endpoint
+    * has already been created above.
+    */
+    wifi_prov_mgr_endpoint_register("thingsboard-url", thingsboard_url_prov_data_handler, NULL);
+
+    wifi_prov_print_qr(service_name, username, pop, PROV_TRANSPORT_SOFTAP);
+}
