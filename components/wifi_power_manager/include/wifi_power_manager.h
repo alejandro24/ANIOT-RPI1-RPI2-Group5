fd8192b40380b83af410923f4f4823564b3e51cb
alejandro24 <alejandrodecelis@gmail.com>
2025-01-29 02:41:49 +0100

    Estandarización del estilo de los comentarios del código.

--- components/wifi_power_manager/include/wifi_power_manager.h
+++ components/wifi_power_manager/include/wifi_power_manager.h
@@ -3,17 +3,17 @@
 
 #include "esp_err.h"
 
-// Enum for Wi-Fi power modes
+/* Enum for Wi-Fi power modes*/
 typedef enum {
     WIFI_POWER_MODE_MIN_MODEM,
     WIFI_POWER_MODE_MAX_MODEM,
     WIFI_POWER_MODE_NONE
 } wifi_power_mode_t;
 
-// Function to initialize the Wi-Fi power manager
+/* Function to initialize the Wi-Fi power manager*/
 esp_err_t wifi_power_save_init(void);
 
-// Function to set the Wi-Fi power mode
+/* Function to set the Wi-Fi power mode*/
 esp_err_t wifi_set_power_mode(wifi_power_mode_t mode);
 
-#endif // WIFI_POWER_MANAGER_H
+#endif /* WIFI_POWER_MANAGER_H*/
