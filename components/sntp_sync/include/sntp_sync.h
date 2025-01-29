bed5180072ce93cb5e4ca8506e6988f9a48c25c0
Pelli223 <dpelli.lafu@gmail.com>
2025-01-29 12:04:02 +0100

    Se arreglan errores de mqtt con respecto al cambio de nevio

--- components/sntp_sync/include/sntp_sync.h
+++ components/sntp_sync/include/sntp_sync.h
@@ -4,10 +4,16 @@
 #include "esp_err.h"
 #include <time.h>
 
+ESP_EVENT_DECLARE_BASE(SNTP_SYNC_EVENT);
+
+typedef enum {
+    SNTP_SUCCESSFULL_SYNC,
+}sntp_sync_event_t;
+
 void obtain_time(void);
 
 void time_sync_notification_cb(struct timeval *tv);
 
-void init_sntp(void);
+void init_sntp(esp_event_loop_handle_t loop);
 
 #endif
