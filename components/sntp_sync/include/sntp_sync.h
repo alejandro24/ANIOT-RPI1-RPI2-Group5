8bb5072d9667dee362c098f3a43275b71da77b02
alejandro24 <alejandrodecelis@gmail.com>
2025-01-29 11:43:17 +0100

    Added missing comments.

--- components/sntp_sync/include/sntp_sync.h
+++ components/sntp_sync/include/sntp_sync.h
@@ -4,10 +4,31 @@
 #include "esp_err.h"
 #include <time.h>
 
+/**
+ * @brief Procedure to obtain the current time.
+ *
+ * @param 
+ * @return
+ *   
+ */
 void obtain_time(void);
 
+/**
+ * @brief Procedure to notify the success time syncronisation.
+ *
+ * @param 
+ * @return
+ *   
+ */
 void time_sync_notification_cb(struct timeval *tv);
 
+/**
+ * @brief Procedure to initialise SNTP to get current time.
+ *
+ * @param 
+ * @return
+ *   
+ */
 void init_sntp(void);
 
 #endif
